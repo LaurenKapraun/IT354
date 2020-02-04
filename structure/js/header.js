435e3b8ee39859c023da9adc1feee3cab1edd70a
Lauren Kapraun <laurenkapraun@gmail.com>
2020-02-04 09:56:16 -0600

    Website 1

---
+++ structure/js/header.js
@@ -0,0 +1,9 @@
+var navbar = document.getElementsByTagName("header");
+
+function openNav() {
+    document.getElementById("navlinks").style.width = "250px";   
+}
+
+function closeNav() {
+    document.getElementById("navlinks").style.width = "0";
+}

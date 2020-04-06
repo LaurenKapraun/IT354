28dd97d445d6ce00cfe692f155636c5af73e4b7e
Lauren Kapraun <38970744+LaurenKapraun@users.noreply.github.com>
2020-04-06 17:44:15 -0500

    Style & Function Updates

---
+++ structure/js/footer.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  if ($(document).height() > $(window).height()) {
+      $('footer').css('position', 'relative');
+      $('footer').css('margin-top', '2em');
+  }
+});
--- structure/js/header.js
+++ structure/js/header.js
@@ -1,9 +1,25 @@
 var navbar = document.getElementsByTagName("header");
 
 function openNav() {
-    document.getElementById("navlinks").style.width = "250px";   
+    document.getElementById("navlinks").style.width = "250px";
 }
 
 function closeNav() {
     document.getElementById("navlinks").style.width = "0";
 }
+
+// Button Styles
+function changeBtnSize() {
+    $(document).ready(function(){
+      var tallestBtn = 0;
+
+      $(this).find('.btn').each(function(){
+        if($(this).height() > tallestBtn){
+            tallestBtn = $(this).height();
+        }
+      })
+        $(this).find('.btn').height(tallestBtn);
+    });
+}
+
+changeBtnSize();
